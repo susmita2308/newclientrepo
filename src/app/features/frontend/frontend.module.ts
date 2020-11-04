c4d24d487674f6eeb9b4072fdad65938e63b5dc2
susmita2308 <susmitapawar1234@gmail.com>
2020-11-04 17:50:27 +0530

    login from

--- src/app/features/frontend/frontend.module.ts
+++ src/app/features/frontend/frontend.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrontendRoutingModule } from './frontend-routing.module';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FrontendRoutingModule
+    FrontendRoutingModule,
+    MaterialModule,
+SharedModule
   ]
 })
 export class FrontendModule { }
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 const modules =[ MatIconModule, MatToolbarModule,
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
   MatListModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatDividerModule
   ]
 @NgModule({
   declarations: [],
