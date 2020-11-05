8e961a6e896fc6cc394cbe36f662ef70201481e9
susmita2308 <susmitapawar1234@gmail.com>
2020-11-05 21:50:04 +0530

    all updated

--- src/app/layouts/frontend/components/resister/resister.component.ts
+++ src/app/layouts/frontend/components/resister/resister.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router, RouterPreloader } from '@angular/router';
+import { SecurityService } from 'src/app/core/services/security.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-resister',
   templateUrl: './resister.component.html',
@@ -10,7 +12,7 @@ export class ResisterComponent implements OnInit {
  
 
   regForm : FormGroup;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder , private securityService:SecurityService ,  private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -34,6 +36,28 @@ export class ResisterComponent implements OnInit {
       ],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.required],
+      address: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(45),
+          Validators.pattern(/a-zA-Z/),
+        ]),
+      ],
     });
   }
+
+register() {
+  this.securityService.register(this.regForm.value).subscribe(
+    (response) => {
+      Swal.fire('registration is succesful', 'you can log in', 'success');
+     
+    },
+    (err) => {
+      Swal.fire('registration is not succesful', "you can't log in", 'error');
+    }
+  );
+}
+
 }
