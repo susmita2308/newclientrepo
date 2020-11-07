95f372d7ad57c1fdc701bdedfef3ee353a3070ca
harshaldhawale <harshal.dhawale@gmail.com>
2020-11-07 10:59:19 +0530

    dynamic update of product

--- src/app/layouts/frontend/components/products/products.component.ts
+++ src/app/layouts/frontend/components/products/products.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  photos=["../../../assets/images/books.jpeg","../../../assets/images/cloths.jpg","../../../assets/images/electronic.jpg"];
 
   constructor() { }
 
